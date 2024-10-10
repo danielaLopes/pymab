6a2c5807e2da2aff5b0a24982d0b473c437a23fd
dlopes <daniela.lopes@lastminute.com>
2024-10-10 23:55:42 +0200

    Changed theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "piccolo_theme"
 html_static_path = ["_static"]
 
 pygments_style = "sphinx"
