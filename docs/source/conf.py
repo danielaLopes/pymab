5623b0e298dc5dbde8e5886fd1b48ed2323e0551
dlopes <daniela.lopes@lastminute.com>
2024-10-25 19:57:22 +0200

    Documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,8 +17,22 @@ release = "0.1.0-alpha.1"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.doctest",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.intersphinx",
     "piccolo_theme",
 ]
+
+autodoc_member_order = 'bysource'
+autodoc_default_options = {
+    'members': True,
+    'member-order': 'bysource',
+    'special-members': '__init__',
+    'undoc-members': True,
+    'exclude-members': '__weakref__'
+}
+
+source_suffix = '.rst'
+source_encoding = 'utf-8-sig'
 
 templates_path = ["_templates"]
 exclude_patterns = []
@@ -28,6 +42,14 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "piccolo_theme"
+html_theme_options = {
+    'globaltoc_collapse': False,
+    'globaltoc_maxdepth': 4,
+    'navigation_with_keys': True,
+}
 html_static_path = ["_static"]
+
+add_module_names = False
+python_use_unqualified_type_names = True
 
 pygments_style = "sphinx"
